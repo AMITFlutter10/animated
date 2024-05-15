96883249f327c94a36e7f06d8f8378ea3ce1cbe2
Esraa <esraa.ramadan667@gmail.com>
2024-05-16 00:08:32 +0300

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+bool isLoading = false;
+String v= "hello group 60";
+
+getData()async{
+  await Future.delayed(Duration(seconds: 5));
+  setState(() {
+    isLoading = true;
+  });
+
+}
+@override
+  void initState() {
+  getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading ?
+      ConWithoutShimmer (x:v):
+      Center(
+        child: Shimmer.fromColors(
+            baseColor: Colors.grey.shade300,
+            highlightColor: Colors.grey.shade100,
+            child: Container(
+              width: 100,
+              height: 200,
+              child:  Text(v, style: TextStyle(
+                fontSize:  20,
+              ),
+              ),
+            )
+          ),
+        ),
+    );
+
+  }
+}
+
+class  ConWithoutShimmer extends StatelessWidget {
+  String x;
+    ConWithoutShimmer({super.key , required this.x});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+            width: 100,
+            height: 200,
+            color:  Colors.pink,
+            child:  Text(x, style: TextStyle(
+              fontSize:  20,
+            ),),
+      ),
+    );
+  }
+}
